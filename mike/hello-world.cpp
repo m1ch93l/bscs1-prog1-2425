02e8294bad2884bb1ec04cd0eb301bc7ddf35775
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-09-04 15:46:37 +0800

    print hello world

---
+++ mike/hello-world.cpp
@@ -0,0 +1,11 @@
+// this line is used to import the iostream standard library, which provides input-output functions, such as cin and cout.
+#include <iostream>
+// this line is used to import the endl function, which is used to print a newline character.
+using namespace std;
+// this line is used to define the main function.
+int main() {
+    // this line is used to print "Hello, world!" to the console.
+  cout << "Hello, world!" << endl;
+  // this line is used to return 0 from the main function.
+  return 0;
+}
