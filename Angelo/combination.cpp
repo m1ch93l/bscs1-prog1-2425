656f59d6446ceeea315f922cb1e3d17f2af8a941
Angelo <angelofoliente0@gmail.com>
2024-09-09 21:23:11 +0800

    Update Angelo

---
+++ Angelo/combination.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+ int main(){
+
+   int num1 = 2;
+   int num2 = 8;
+   int results = num1+num2;
+   
+   cout << "COMBINATION" <<endl;
+   cout << "Number1:"  << num1 <<endl;
+   cout << "Number2:" << num2 <<endl;
+   cout << "Results:" << results <<endl;
+
+return 0;
+}
