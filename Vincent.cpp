e86b22ab7304ff3dadb5a540a2171807d255b58d
Vicntez2006 <tadoranvincent5@gmail.com>
2024-09-09 14:37:51 +0800

    Create Vincent.cpp

---
+++ Vincent.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string firstName = "vincent";
+  string middleName = "monteza";
+  string lastName = "tadoran";
+
+  string fullName;
+
+  fullName = firstName + " " + middleName + " " + lastName;
+
+  cout << fullName << endl;
+
+  return 0;
+}
+
+
+
+    
