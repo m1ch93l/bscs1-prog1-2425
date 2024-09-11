9289ef074596555d33bf5565ae48534f197e45dd
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-09-11 22:40:12 +0800

    Add user input.

--- mike/data-type.cpp
+++ mike/data-type.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+
+  int number; // explicit variable
+  int number = 2; // implicit variable
+  int x = number + 1; //anonymous variable
+
   int intNum = 1;
   float floatNum = 10.5;
-  double doubleNum = 10.5;
+  double doubleNum = 10.4;
   char charLetter = 'A';
   bool boolValue = false;
   string stringText = "Hello, world! 3897428974";
