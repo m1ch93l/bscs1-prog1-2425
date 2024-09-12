398fd0a5e11807d75b1c64a240b1081c587d0a88
Jemar Praxides <jmarpraxides@gmail.com>
2024-09-12 13:55:49 +0800

    Create Praxides 4.cpp

---
+++ Jemar/Praxides 4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+   
+
+int main() {
+
+ int A = 80;
+ int B = 90;
+ int C = 95;
+ int D = 85;
+
+ float division = 4;
+ 
+ int add =  A+B+C+D;
+float RESULT = add / division;
+
+cout << "PRELIM: " << A << endl;
+cout << "MIDTERM: "<< B << endl;
+cout << "PREFINAL " << C << endl;
+cout << "FINAL: " << D << endl;
+cout << "AVERAGE: " << RESULT << endl;
+ 
+
+return 0;
+}
