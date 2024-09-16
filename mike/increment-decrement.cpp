6b143f9286a0c555ffe23e829178128c58117604
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-09-16 13:07:49 +0800

    Increment and Decrement.

---
+++ mike/increment-decrement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num1 = 10;
+    int num2 = 10;
+
+    num1++; // plus one
+    num2--; // minus one
+    
+    cout << num1 << endl;
+    cout << num2 << endl;
+
+    return 0;
+}
