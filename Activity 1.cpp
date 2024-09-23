74554995365403611c4ec1a1b5fd75660570796d
Suzette-jpg <suzettesacueza072003@gmail.com>
2024-09-23 14:44:21 +0800

    Update

---
+++ Activity 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string firstname =  "Suzette";
+string middlename =  "Ibo";
+string lastname =  "Sacueza";
+
+string fullname ;
+
+fullname = firstname + " " + middlename + " " + lastname;
+
+cout <<  fullname << endl ;
+
+return 0;
+}
