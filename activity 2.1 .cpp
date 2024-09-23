bfd43c6cf548f4849f60bd3b8d55ed26e120325d
Suzette-jpg <suzettesacueza072003@gmail.com>
2024-09-23 14:47:25 +0800

    Update

---
+++ activity 2.1 .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string fullname = "Suzette I. Sacueza";
+    string course = "A.C.T 1";
+    cout << fullname << endl;
+    cout << course << endl;
+    return 0;
+}
