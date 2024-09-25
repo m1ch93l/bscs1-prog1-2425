c212c519d51e9060e01be61f6db1719438c5d0a2
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-09-25 12:38:59 +0800

    While and do-while loop.

--- mike/do-while-loop.cpp
+++ mike/do-while-loop.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 
     int number = 1;
 
-    while (number < 10) {
+    do {
         cout << number << ".)" << endl;
         number++;
-    }
+    } while(number < 10);
 
     return 0;
   }
