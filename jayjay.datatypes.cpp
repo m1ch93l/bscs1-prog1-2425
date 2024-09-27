953103d87683cc602f9b33f7407b5e1868b6a5a7
Jay-jay Tirol <tiroljayjay1@gmail.com>
2024-09-27 11:19:12 +0800

    Update jayjay.datatypes.cpp

---
+++ jayjay.datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int Age = 18;
+    string Fullname = "Jayjay mendoza tirol";
+    string Course = "Act-1";
+    string Gender = "Male";
+
+    cout << "Students Information!" << endl;
+    cout << "Fullname:" << Fullname << endl;
+    cout << "Age:" << Age << endl;
+    cout << "Course:" << Course << endl;
+    cout << "Gender:" << Gender << endl;
+    
+    
+    return 0;
+}
