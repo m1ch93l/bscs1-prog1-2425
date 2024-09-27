4fa602006827fa6de57802e98ca6e167833db39f
Jay-jay Tirol <tiroljayjay1@gmail.com>
2024-09-27 11:15:15 +0800

    Update jayjay.gradingsystem.cpp

---
+++ jayjay.gradingsystem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+   int prelim = 80;
+   int midterm = 90;
+   int prefinal = 95;
+   int final = 85;
+ 
+  string stringText = "Average: 87.5";
+
+   cout << "prelim: " << prelim << endl;
+   cout << "midterm:" << prelim << endl;
+   cout << "prefinal:" << prelim << endl;
+   cout << "final:" << prelim << endl;
+
+   cout << "Average 87.5 " << endl;
+    return 0;
+}
