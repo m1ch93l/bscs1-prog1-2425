ea1fc1856ac7120f84ecf301848ad8898731fd0e
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2024-10-06 00:20:42 +0800

    Create praxides5.cpp

---
+++ Jemar/praxides5.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        
+    string firstName;
+    string middleName;
+    string lastName;
+
+    cout << "Activity 1: " << endl;
+    cout << "" << endl;
+
+    cout << "firstName: ";
+    cin >> firstName;
+
+    cout << "middleName: ";
+    cin >> middleName;
+
+    cout << "lastName: ";
+    cin >> lastName;
+
+    string fullName = firstName + " " + middleName + " " + lastName;
+
+    cout << "FullName: " << fullName;
+    cout << "" << endl;
+  
+  double prelim;
+  double midterm;
+  double prefinal;
+  double final;
+    
+  cout << "Activity 2: " << endl;
+  cout << "" << endl;
+  
+  cout << "My Grades: " << endl;
+  
+  cout << "Prelim: ";
+  cin >> prelim;
+  
+  cout << "Midterm: ";
+  cin >> midterm;
+  
+  cout << "Prefinal: ";
+  cin >> prefinal;
+  
+  cout << "Final: ";
+  cin >> final;
+  
+  double add = prelim + midterm + prefinal + final;
+  double divide = add / 4;
+  
+  cout << "" << endl;
+  cout << "Average: " << divide << endl;
+  cout << "" << endl;
+    
+    return 0;
+}
