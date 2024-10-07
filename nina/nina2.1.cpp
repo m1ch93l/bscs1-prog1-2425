d5f2f2477cf51609aeed785e7c85619f1022dbbe
NINA <ninajoy2006@gmail.com>
2024-10-07 15:28:27 +0800

    Update nina

---
+++ nina/nina2.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string fullname = "Nina Joy D. Parica";
+    string course = "BSCS 1";
+    cout << fullname << endl;
+    cout << course << endl;
+    return 0;
+}
