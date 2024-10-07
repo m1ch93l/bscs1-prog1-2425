5c21c1c11dbe790371791ed2b2993c92c786f10d
Jay-jay Tirol <tiroljayjay1@gmail.com>
2024-10-07 16:08:08 +0800

    Create activity8.cpp

---
+++ JAYJAY/activity8.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+ using namespace std;
+
+  int main(){
+
+    string name = "jayjay";
+    char middle = 'm';
+    string last = "tirol";
+    int age = 18;
+    bool boolvalue = true;
+    double height = 180;
+    float weight = 56;
+    
+    cout << "Name:" << name << endl;
+    cout << "MiddleName:" << middle << endl;
+    cout << "LastName:" << last << endl;
+    cout << "Age:" << age << endl;
+    cout << "Male:" << boolvalue << endl;
+    cout << "Height(cm):" << height << endl;
+    cout << "Weight(kg):" << weight << endl;
+    
+    cout << "_____________________" <<endl;
+    
+    string firstname = "jayjay";
+    string middlename = "m.";
+    string lastname = "tirol";
+
+    string full;
+
+    full = firstname +" "+ middlename +" "+ lastname;
+
+    cout << "ACTIVITY 1" <<endl;
+    cout << "CONCAT" <<endl;
+    cout << "Name:" << full <<endl;
+
+    cout << "_____________________" <<endl;
+
+    string First = "jayjay ";
+    string Middle = "m.";
+    string Last = " tirol ";
+    string course= "ACT-";
+    int intNum = 1;
+ 
+    string fullname;
+ 
+    fullname = First+Middle+Last;
+    
+    cout << "ACTIVITY 2" << endl;
+    cout << "NAME AND COURSE" << endl;
+    cout << "Name:" << fullname <<endl;
+    cout << "Course:" << course;
+    cout << intNum <<endl;
+
+   cout << "_____________________" <<endl;
+   
+   int num1 = 2;
+   int num2 = 8;
+   int results = num1+num2;
+   
+   cout << "ACTIVITY 3" <<endl;
+   cout << "COMBINATION" <<endl;
+   cout << "Number1:"  << num1 <<endl;
+   cout << "Number2:" << num2 <<endl;
+   cout << "Results:" << results <<endl;
+   
+   cout << "_____________________" <<endl;
+   
+  int prelim = 80;
+  int midterm = 90;
+  int prefinal = 95;
+  int final = 85;
+  int average = prelim+midterm+prefinal+final;
+  float divide = 4;
+  float result = average/divide;
+  
+    cout << "ACTIVITY 4" <<endl;
+    cout << "Grades" <<endl;
+    cout << "Prelim:" << prelim << endl;
+    cout << "Midterm:" << midterm << endl;
+    cout << "Prefinal:" << prefinal << endl;
+    cout << "Final:" << final << endl;   
+    cout << "Average:" << result << endl;
+  
+      
+    return 0;
+    }
+        
