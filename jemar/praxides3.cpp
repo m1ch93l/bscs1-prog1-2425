9d164195ca4eff6e978574fa1082d92fab315a88
jemar-ux <jemarpraxides653@gmail.com>
2024-10-07 16:10:59 +0800

    Create praxides3.cpp

---
+++ jemar/praxides3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main ()
+{
+	int a, b, sum;
+	
+	std::cout << "Enter first number: " << '\n';
+	std::cin >> a;
+	
+	std::cout << "Enter second number: " << '\n';
+	std::cin >> b;
+	
+
+	sum = a + b;
+	
+	std::cout << "The sum is: " << sum <<'\n';	
+	return 0;
+}
