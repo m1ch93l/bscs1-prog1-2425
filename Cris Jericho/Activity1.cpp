a16a6ca560c65ae397eb2f8fccbabe154670b3fe
Dudster153 <thelastguest0033@gmail.com>
2024-10-07 20:06:16 +0800

    Create Activity1.cpp
    Finally manage to submit hahahaha

---
+++ Cris Jericho/Activity1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string firstname =  "Cris";
+string secondname = "Jericho";
+string middlename =  "Guevarra";
+string lastname =  "Samper";
+
+string fullname ;
+
+fullname = firstname + " " + secondname + " " + middlename + " " + lastname;
+
+cout <<  fullname << endl ;
+
+return 0;
+}
