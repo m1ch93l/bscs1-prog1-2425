1e826d46a3eb10c8932683a1378bc593e147834b
4rab3la17 <arabelataduran460@gmail.com>
2024-10-07 18:44:23 -0700

    Create nameandage.cpp

---
+++ Arabela/nameandage.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+ string name = "Arabela";
+ char middle = 'I';
+ string last = "Taduran";
+ int age = 18;
+ 
+ cout <<"Name:" << name << endl;
+ cout <<"MiddleName:" << middle << endl;
+ cout <<"LastName:" << last << endl;
+ cout <<"Age:" << age << endl;
+
+
+ return 0;
+ }
