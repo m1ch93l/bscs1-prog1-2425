8cf4478a9c30e0cfb8d0a48ec2fcc9e27f526af1
V1cnt3z <tadoranvincent5@gmail.com>
2024-10-09 19:45:20 +0800

    Create 1.cpp

---
+++ V1CNT3Z/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string stringText = "Jhondean"; 
+    int intNum = 12;
+    float floatNum = 1.2;
+    double doubleNum = 1.69;
+    char charLetter = 'V';
+    bool boolValue = false;
+
+    cout << "Name:" << stringText << endl;
+    cout << "Age:" << intNum << endl;
+    cout << "Coke:" << floatNum << endl;
+    cout << "Sprite:" << doubleNum << endl;
+    cout << "1st Letter:" << charLetter << endl;
+    cout << "Human?" << boolValue << endl;
+
+    return 0;
+
+}
