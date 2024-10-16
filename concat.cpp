3682811ac86adb3bbc0ded496c0bc5c130cfc60c
Kaithyhen <Kaithpaulinecayas@gmail.com>
2024-10-16 16:37:32 +0800

    Create concat.cpp

---
+++ concat.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string firstname =  "Kaith";
+string middlename =  "Cayas";
+string lastname =  "Occiano";
+
+string fullname ;
+
+fullname = firstname + " " + middlename + " " + lastname;
+
+cout <<  fullname << endl ;
+
+return 0;
+}
