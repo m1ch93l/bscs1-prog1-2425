522424a2711a374ebcd5ca1491fedcad24f62e07
4rab3la17 <arabelataduran460@gmail.com>
2025-01-14 17:43:04 +0800

    myproject.cpp

---
+++ Arabela/myproject.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+ using namespace std;
+
+  int main(){
+
+    string name = "Arabela";
+    char middle = 'I';
+    string last = "Taduran";
+    int age = 18;
+    bool boolvalue = true;
+    double height = 162.5;
+    float weight = 45;
+    
+    cout << "Name: " << name << endl;
+    cout << "MiddleName: " << middle << endl;
+    cout << "LastName: " << last << endl;
+    cout << "Age:" << age << endl;
+    cout << "Female:" << boolvalue << endl;
+    cout << "Height(cm): " << height << endl;
+    cout << "Weight(kg): " << weight << endl;
+    
+    
+    return 0;
+  }
